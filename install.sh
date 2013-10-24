664f4d7518862d45803e0923aeb8150737df9aa0
Kenneth Lim <kenlimmj@gmail.com>
2013-10-23 23:48:16 -0400

    Wrote an install script to copy the files to the system directory. From now on it is possible to use \usepackage{dominatrix} instead of just specifying the full file path

---
+++ install.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+echo "DOMINATRIX PACKAGE INSTALLATION SCRIPT"
+echo "This bash script copies the dominatrix package files to your LaTeX user folder. You may be prompted for your system password to enable copying."
+
+# Use the inbuilt kpsewhich function to hunt down the system texmf directory
+echo "Detecting TeX directory..."
+TEXDIR=`kpsewhich -var-value=TEXMFHOME`
+
+# Check to make sure that TEXDIR is set before copying
+if [[ -z $TEXDIR ]]
+then
+  echo "No TeX directory detected. Unable to proceed. Exiting..."
+  exit 1
+else
+  # Set up the ../tex/latex filepath if it doesn't already exist
+  echo "Creating filepaths..."
+  sudo mkdir -p $TEXDIR/tex/latex
+
+  # Recursively copy the appropriate directories in the current directory to the system directory
+  echo "Copying files..."
+  sudo cp -R -p ${0%/*}/dominatrix $TEXDIR/tex/latex/
+  sudo cp -R -p ${0%/*}/atbeginend $TEXDIR/tex/latex/
+
+  echo "Done!"
+fi
+
+echo "Rehashing TeX packages so the system sees the new package"
+sudo texhash
